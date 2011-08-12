1d2bb8087c241c7ba4189afdf9ba51dd961182e5
Joel Cofie <jtcofie@gmail.com>
2011-08-12 15:08:51 +0000

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Linkup::Application.config.secret_token = '802ddcfdf1b5036dd63cfbf191448eb6bd0c2545e7f20f53872a6ad71b203d503eae471e0fcd4a4e5cb7144c85fb89a09aca1a0e187f4d58a7c7279187bcfb8b'
